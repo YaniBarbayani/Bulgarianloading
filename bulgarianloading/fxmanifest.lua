eaeb866b8028e66c955ff6c283951654b89ffee4
FiveM Support <121999573+YaniBarbayani@users.noreply.github.com>
2026-08-01 21:57:38 +0300

    Add files via upload

---
+++ bulgarianloading/fxmanifest.lua
@@ -0,0 +1,18 @@
+----------------------------------
+--<!>-- Mr.Shellby-- 5M Leaks Bulgaria --<!>--
+----------------------------------
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'loadingscreen'
+
+version '1.0'
+
+loadscreen 'html/loadscreen.html'
+
+files {
+    "html/**/*"
+}
+dependency '/assetpacks'
